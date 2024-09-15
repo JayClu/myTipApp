72e227c10e7df627a97255fed3849e8c2daf2a74
hoangloc <hoangloc2502@gmail.com>
2024-09-15 17:52:42 +0700

    setting up the container function, create input fields

---
+++ app/src/main/java/hoangloc/mytipapp/MainActivity.kt
@@ -0,0 +1,134 @@
+package hoangloc.mytipapp
+
+import android.os.Bundle
+import android.support.v4.os.IResultReceiver2.Default
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import hoangloc.mytipapp.components.InputField
+import hoangloc.mytipapp.ui.theme.MyTipAppTheme
+
+@ExperimentalComposeUiApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Myapp{
+//                TopHeadler()
+                MainContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun Myapp(content: @Composable () -> Unit) {
+    MyTipAppTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun TopHeadler(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(15.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFF62BE87)
+        ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall)
+            Text(text = "$$total",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold)
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("0")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+        ){
+        Column {
+
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions() {
+                    if (!validState) return@KeyboardActions
+                        //todo- onvaluechanged
+                    keyboardController?.hide()
+                }
+            )
+            //OutlinedTextField(value = , onValueChange = )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyTipAppTheme {
+        Myapp {
+            Text(text = "hello again")
+        }
+    }
+}
